f709a5419ea8027a66939eb2fb33bec3eb866503
Julian Ng <julesnuggy@gmail.com>
2023-11-02 08:13:52 +0000

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import './App.css'
 function AdviceSection() {
   return (
     <section>
-      <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <h2>Art Section</h2>
+      <section className="art-list"></section>
     </section>
   )
 }
